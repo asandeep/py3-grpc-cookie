82de219340d202c322f8d7f707aa6f0bc9842147
Sandeep Aggarwal <asandeep.me@gmail.com>
2020-03-08 19:16:46 +0530

    Renamed .gitignore->.gitignore.tpl to prevent project files from getting ignored and updated post gen hook to rename template files.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -7,6 +7,11 @@ PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 def remove_file(filepath):
     os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
 
+def rename_file(filepath, new_filepath):
+    abs_original_filepath = os.path.join(PROJECT_DIRECTORY, filepath)
+    abs_new_filepath = os.path.join(PROJECT_DIRECTORY, new_filepath)
+    os.rename(abs_original_filepath, abs_new_filepath)
+
 
 if __name__ == "__main__":
 
@@ -15,3 +20,5 @@ if __name__ == "__main__":
 
     if "Not open source" == "{{ cookiecutter.open_source_license }}":
         remove_file("LICENSE")
+
+    rename_file(".gitignore.tpl", ".gitignore")
